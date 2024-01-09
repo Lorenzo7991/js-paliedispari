b86e83bffccaae0a4d7aef8777acda6cdf35cd54
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-09 19:19:20 +0100

    add palindromic JS function

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,7 @@
-console.log('JS OK');
+console.log('functions-JS OK');
+
+function isPalindrome(input) {
+    const customInput = input.trim().toLowerCase();
+    const reverseInput = customInput.split('').reverse('').join('');
+    return customInput === reverseInput;
+}
--- js/palindromic.js
+++ js/palindromic.js
@@ -1,1 +1,18 @@
-console.log('pali-JS-OK');
+console.log('palindromic-JS-OK');
+
+//* Requiring user word imput
+let userWord = prompt('Inserire la parola da verificare:', 'Anna').trim();
+
+console.log(userWord);
+
+//* Validating user imput
+if (!isNaN(userWord) || userWord.length < 3) {
+    console.error("La parola inserira non rispetta i parametri richiesti, assicurati di aver inserito una PAROLA e che sia ALMENO PIU' LUNGA DI TRE CARATTERI");
+} else {
+
+    if (isPalindrome(userWord)) {
+        console.log('La parola inserita è palidroma!')
+    } else {
+        console.log('la parola inserita non è palindroma')
+    }
+}
