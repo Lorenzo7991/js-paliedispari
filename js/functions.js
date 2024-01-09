3602bb325720e7c127f67edf26c45cc902caf02e
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2024-01-09 19:25:37 +0100

    fix JS comments

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,10 @@
 console.log('functions-JS OK');
 
+/**
+ * Checking if the word entered by the user is a palidrome or not
+ * @param {string} input //User-entered imput parameter
+ * @returns //The word checked
+ */
 function isPalindrome(input) {
     const customInput = input.trim().toLowerCase();
     const reverseInput = customInput.split('').reverse('').join('');
--- js/palindromic.js
+++ js/palindromic.js
@@ -9,7 +9,7 @@ console.log(userWord);
 if (!isNaN(userWord) || userWord.length < 3) {
     console.error("La parola inserira non rispetta i parametri richiesti, assicurati di aver inserito una PAROLA e che sia ALMENO PIU' LUNGA DI TRE CARATTERI");
 } else {
-
+    //*Calling the function for word check
     if (isPalindrome(userWord)) {
         console.log('La parola inserita è palidroma!')
     } else {
